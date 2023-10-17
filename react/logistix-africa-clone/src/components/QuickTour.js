e5ca76441abf419c2a784f38f862998a8da9fb4c
Osijonah <osijonah@gmail.com>
2023-10-17 12:56:34 +0100

    functionality of quicktour section

--- react/logistix-africa-clone/src/components/QuickTour.js
+++ react/logistix-africa-clone/src/components/QuickTour.js
@@ -1,8 +1,24 @@
 import "../css/QuickTour.css";
-import tracker from "../imgs/tracker.png"
+import { quicktourList } from "../objects/quickTourData";
+// import tracker from "../imgs/tracker.png";
 import React from "react";
+import { useState } from "react";
 
 const QuickTour = () => {
+
+    const [index, setIndex] = useState(0);
+
+    const handlePreviousClick = (e) => {
+        e.preventDefault();
+        setIndex(index - 1);
+    }
+    const handleNextClick = (e) => {
+        e.preventDefault();
+        setIndex(index + 1);
+    }
+
+    let quicktour = quicktourList[index];
+
     return(
         <div class="quick-tour-div">
             <h1>A Quick Tour.</h1>
@@ -11,17 +27,17 @@ const QuickTour = () => {
                 <div>
                     <div class="quick-tour-btn-div">
                         <div class="prev-next-div">
-                            <a href="/" id="prev" class="prev prev-next">← PREV</a>
+                            <a href="/" id="prev" onClick={handlePreviousClick} class="prev prev-next">← PREV</a>
                             <span class="prev-next-span">1 of 5</span>
-                            <a href="/" class="next prev-next">NEXT →</a>
+                            <a href="/" onClick={handleNextClick} class="next prev-next">NEXT →</a>
                         </div>
                         <hr/>
                         <div class="quick-tour-txt-div">
                             <div class="qtext1">
-                                <div class="quick-tour-text">
-                                    <h3>Track Parcels</h3>
+                                <div class="    quick-tour-text">
+                                    <h3>{quicktour.header}</h3>
                                     <p>
-                                        Your <span class="medium1">Customers can Track Their Parcels</span> from Pickup to Destination. They get an estimated delivery time for every parcel they ship using a Tracking ID that will be sent to their mail after registering their parcels.
+                                        {quicktour.para}
                                     </p>
                                 </div>
                             </div>
@@ -38,12 +54,13 @@ const QuickTour = () => {
                         </div>
                     </div>
                     <div class="quick-tour-img-div img1">
-                        <img src={tracker} alt=""/>
+                        <img src={quicktour.img} alt={quicktour.alt}/>
                     </div>
                 </div>
             </section>
         </div>
     );
+
 }
 
 export default QuickTour;
---
+++ react/logistix-africa-clone/src/objects/quickTourData.js
@@ -0,0 +1,32 @@
+import administrators from "../imgs/administrators.png";
+import tracker from "../imgs/tracker.png";
+import dispatchers from "../imgs/dispatchers.png";
+import orders from "../imgs/orders.png";
+import pricelist from "../imgs/pricelist.png";
+
+export const quicktourList = [{
+    header: "Track Parcels",
+    para: 'Your <span class="medium1">Customers can Track Their Parcels</span> from Pickup to Destination. They get an estimated delivery time for every parcel they ship using a Tracking ID that will be sent to their mail after registering their parcels.',
+    img: tracker,
+    alt: "tracker"
+}, {
+    header: "Receive & Manage Orders",
+    para: 'We made it possible that You, as well as your Administrators can <span class="medium1">Manage Shipments And Monitor Parcels</span> from Pickup to Destination. You also have the ability to <span class="medium1">Manage your Customers’ information</span> so you can always be in communication with your customers.',
+    img: orders,
+    alt: "orders"
+}, {
+    header: "Control Your Price List",
+    para: 'We understand different companies have their unique pricing models, so we designed Logistix with this need in mind. And so we have created a unique way to <span class="medium1">Accommodate for any pricing model of choice.</span> ',
+    img: pricelist,
+    alt: "pricelist"
+}, {
+    header: "Manage Your Dispatchers",
+    para: 'We also provide a way to <span class="medium1">Manage your Dispatch Riders.</span> By manage, we mean you can <span class="medium1">Create</span> new riders, Assign Parcels to dispatch riders, <span class="medium1">Edit</span> and <span class="medium1">Delete</span> dispatch riders.',
+    img: dispatchers,
+    alt: "dispatchers"
+}, {
+    header: "Manage Your Administrators",
+    para: 'We designed it with the ability to allow you <span class="medium1">Delegate Administrative Tasks</span> such as the management of shipments and dispatch riders to administrators so you can focus on growing your business.',
+    img: administrators,
+    alt: "administrators"
+}]
